5c04604288b96d1f0e787ae1fd6c0c37c37e4a0d
jamalondon <brysonlondon97@live.com>
2026-01-25 18:20:53 -0600

    .

--- src/controllers/sermonController.js
+++ src/controllers/sermonController.js
@@ -1,6 +1,7 @@
 const supabase = require('../supabase');
 const AppError = require('../utils/AppError');
-const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const uuidRegex =
+	/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 exports.createSermon = async (req, res) => {
 	try {
