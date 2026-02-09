ea300f1cd36d43648c1c71108cc24c68fc1de31b
jamalondon97 <brysonlondon97@live.com>
2026-02-09 14:49:17 -0600

    Slight change to user controller logic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 // Initialize Supabase client
 const supabase = createClient(
 	process.env.SUPABASE_URL,
-	process.env.SUPABASE_SERVICE_KEY
+	process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 /*
